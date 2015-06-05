65c49c472f232328aa250e70eb6562219b8c6e29
Daniel Chen <Maveri2k@yahoo.com>
2015-06-05 00:06:40 -0400

    emacs and package updates

---
+++ .emacs.d/elpa/pdf-tools-20150603.227/pdf-tools-pkg.el
@@ -0,0 +1,8 @@
+(define-package "pdf-tools" "20150603.227" "Support library for PDF documents."
+  '((emacs "24.3")
+    (tablist "0.70"))
+  :keywords
+  '("files" "multimedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/tablist-20150602.827/tablist-autoloads.el
@@ -0,0 +1,18 @@
+;;; tablist-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("tablist-filter.el" "tablist-pkg.el" "tablist.el")
+;;;;;;  (21871 42966 228980 7000))
+
+;;;***
+
+(provide 'tablist-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tablist-autoloads.el ends here
