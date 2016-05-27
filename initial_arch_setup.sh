7b6f38cb4453dd590e3a9ea9915087ede3c411c2
Daniel Chen <chendaniely@users.noreply.github.com>
2016-05-27 14:41:48 -0400

    Update initial_arch_setup.sh

--- initial_arch_setup.sh
+++ initial_arch_setup.sh
@@ -2,6 +2,9 @@ sudo mount -o remount,size=16G,noatime /tmp
 
 yaourt -Syyua pacaur
 
+# might need to run `gpg --recv-keys <key>`
+# `gpg --recv-keys 1EB2638FF56C0C53`
+
 pacaur -Syyua emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
     git \
     base-devel xf86-input-synaptics \
