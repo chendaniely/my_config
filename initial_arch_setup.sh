791f97f09f051bd856381c83894e90f1284b952d
Daniel Chen <chendaniely@users.noreply.github.com>
2016-05-27 13:36:40 -0400

    synology-cloud-station-drive

--- initial_arch_setup.sh
+++ initial_arch_setup.sh
@@ -3,4 +3,5 @@ sudo mount -o remount,size=16G,noatime /tmp
 yaourt -Syyua emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
     git \
     base-devel xf86-input-synaptics \
-    lxc arch-install-scripts
+    lxc arch-install-scripts \
+    synology-cloud-station-drive
