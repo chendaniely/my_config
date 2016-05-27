540c461a5333ba1a690dd47ea5f3dd92eaf55b18
Daniel Chen <chendaniely@users.noreply.github.com>
2016-05-27 14:46:23 -0400

    no `a` flag in pacaur

--- initial_arch_setup.sh
+++ initial_arch_setup.sh
@@ -5,7 +5,7 @@ yaourt -Syyua pacaur
 # might need to run `gpg --recv-keys <key>`
 # `gpg --recv-keys 1EB2638FF56C0C53`
 
-pacaur -Syyua emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
+pacaur -Syyu emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
     git git-cola gitkraken \
     base-devel xf86-input-synaptics \
     lxc arch-install-scripts \
