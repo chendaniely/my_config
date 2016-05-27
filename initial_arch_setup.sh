bdc7fd1e3639d1ff9644d84c4dfbe66384cb8db3
Daniel Chen <chendaniely@users.noreply.github.com>
2016-05-27 13:49:27 -0400

    install pacaur and then use pacaur to install

--- initial_arch_setup.sh
+++ initial_arch_setup.sh
@@ -1,6 +1,8 @@
 sudo mount -o remount,size=16G,noatime /tmp
 
-yaourt -Syyua emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
+yaourt -Syyua pacaur
+
+pacaur -Syyua emacs r gcc-fortran rstudio-desktop-preview-bin emacs-ess openblas intel-mkl r-mkl intel-advisor-xe \
     git \
     base-devel xf86-input-synaptics \
     lxc arch-install-scripts \
