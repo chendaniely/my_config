7ba89ff4a2458cb143f764e2eb559dbc3cc17bfb
Daniel Chen <chendaniely@users.noreply.github.com>
2016-06-09 09:47:37 -0400

    add reflector to find fastest mirrors

--- initial_arch_setup.sh
+++ initial_arch_setup.sh
@@ -1,6 +1,8 @@
 sudo mount -o remount,size=16G,noatime /tmp
 
-yaourt -Syyua pacaur
+yaourt -Syyua pacaur reflector
+
+reflector --verbose -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 # might need to run `gpg --recv-keys <key>`
 # `gpg --recv-keys 1EB2638FF56C0C53`
