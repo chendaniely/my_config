cfe1454f78d23e701d8f037fcce0bd586cb8c4fd
Daniel Chen <chendaniely@users.noreply.github.com>
2016-07-13 16:54:36 -0500

    conda install nbpresent

--- conda_install.sh
+++ conda_install.sh
@@ -3,3 +3,4 @@ conda install ncurses
 # symbol lookup error: /usr/lib/libpangoft2-1.0.so.0: undefined symbol: FcWeightFromOpenType
 # https://github.com/ContinuumIO/anaconda-issues/issues/368
 conda install -c asmeurer pango
+conda install -c conda-forge nbpresent
