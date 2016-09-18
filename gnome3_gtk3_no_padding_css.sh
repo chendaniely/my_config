0156cf77cd4ebad42da1894543fbab41f790a1a6
Daniel Chen <chendaniely@users.noreply.github.com>
2016-09-18 09:11:53 -0400

    Create gnome3_gtk3_no_padding_css.sh

---
+++ gnome3_gtk3_no_padding_css.sh
@@ -0,0 +1,12 @@
+echo "window.ssd headerbar.titlebar {
+    padding-top: 4px;
+    padding-bottom: 4px;
+    min-height: 0;
+}
+
+window.ssd headerbar.titlebar button.titlebutton {
+    padding: 0px;
+    min-height: 0;
+    min-width: 0;
+}
+" > ~/.config/gtk-3.0/gtk.css
