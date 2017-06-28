4c88d29acabfda7b0e1a19d22c90fbf0fecd0abc
Daniel Chen <chend@vt.edu>
2017-06-28 18:31:20 -0400

    fix aliases and add print staments

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
+# backup old files
+echo "backing up old files"
 mv ~/.bashrc ~/.bashrc_bkup
 mv ~/.bash_aliases ~/.bash_aliases_bkup
-
-# mv ~/.emacs ~/.emacs_bkup
+mv ~/.emacs ~/.emacs_bkup
 mv ~/.emacs.d ~/.emacs.d_bkup
 
+# create new links
+echo "creating symlinks"
 ln -s ${PWD}/.bashrc ~/.bashrc
-ln -s ${PWD}/bash_aliases ~/.bash_aliases
-
+ln -s ${PWD}/.bash_aliases ~/.bash_aliases
 # ln -s ${PWD}/.emacs ~/.emacs
 ln -s ${PWD}/.emacs.d ~/.emacs.d
 
-
 # git config --global alias.l 'log --oneline --graph --decorate --all'
+
+echo "Done"
