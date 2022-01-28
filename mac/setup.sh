45549585b6569eb70ac1a5cbbd4b6e812dfe7a19
Daniel Chen <chendaniely@users.noreply.github.com>
2022-01-27 22:45:40 -0500

    brew install --cask iterm2

--- mac/setup.sh
+++ mac/setup.sh
@@ -15,6 +15,7 @@ brew install --cask emacs vscodium
 ## brew install other stuff
 
 brew install --cask firefox-developer-edition
+brew install --cask iterm2
 # install screen recorder
 brew install --cask kap
 # window manager
