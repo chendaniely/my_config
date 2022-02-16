59f6f2e4ec8e01f814f7d930151b720a66ea1339
Daniel Chen <chendaniely@users.noreply.github.com>
2022-02-15 20:13:07 -0500

    texstudio zotero-beta

--- arch_linux/initial_arch_setup.sh
+++ arch_linux/initial_arch_setup.sh
@@ -41,6 +41,7 @@ $AUR_HELPER -Syu --noconfirm arch-install-scripts gcc-fortran gcc clang git svn 
     
 $AUR_HELPER -Syu emacs-ess
 sudo pacman -Syu --noconfirm texlive-most texlive-lang texlive-langextra texlive-bibtexextra texlive-fontsextra biber
+$AUR_HELPER -Syu --noconfirm texstudio zotero-beta 
 $AUR_HELPER -Syu --noconfirm obs-studio kdenlive ffmpeg imagemagick vlc
 
 ## Docker
