c1d4af2da99a03648cc41faf4b3298a4b02e79d9
Daniel Chen <chendaniely@users.noreply.github.com>
2022-04-19 11:44:37 -0700

    get vscode marketplace working

--- arch_linux/initial_arch_setup.sh
+++ arch_linux/initial_arch_setup.sh
@@ -27,7 +27,7 @@ $AUR_HELPER -Syu --noconfirm
 #reflector --verbose -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 $AUR_HELPER -Syu --noconfirm arch-install-scripts gcc-fortran gcc clang git svn base-devel \
-    emacs code code-marketplace \
+    emacs code code-marketplace code-features \
     openblas intel-mkl \
     r tk rstudio-desktop-daily-bin quarto-cli-bin \
     pandoc-bin \
