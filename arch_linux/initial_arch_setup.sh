724033aa1b29448b7f556afda15f62c01d886bef
Daniel Chen <chendaniely@users.noreply.github.com>
2022-05-29 12:40:09 -0400

    use powertop for power management

--- arch_linux/initial_arch_setup.sh
+++ arch_linux/initial_arch_setup.sh
@@ -45,9 +45,11 @@ $AUR_HELPER -Syu --noconfirm arch-install-scripts gcc-fortran gcc clang git svn 
     yakuake
 
 # power management
-yay -Syu --noconfirm tlp tp_smapi acpi_call
-sudo systemctl start tlp.service
-sudo systemctl enable tlp.service
+#yay -Syu --noconfirm tlp tp_smapi acpi_call
+#sudo systemctl start tlp.service
+#sudo systemctl enable tlp.service
+# https://www.reddit.com/r/archlinux/comments/rujhv0/best_power_management_solution/
+sudo pacman -Syu powertop
     
 $AUR_HELPER -Syu emacs-ess
 sudo pacman -Syu --noconfirm texlive-most texlive-lang texlive-langextra texlive-bibtexextra texlive-fontsextra biber
